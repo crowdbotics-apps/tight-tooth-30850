3e2e9aff0acbc5684395f5c1a524b858348a5665
Crowdbotics <team@crowdbotics.com>
2021-09-24 23:05:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tight_tooth_30850",
+  url: "https://tight_tooth_30850.botics.co",
+  api: "https://tight_tooth_30850.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
